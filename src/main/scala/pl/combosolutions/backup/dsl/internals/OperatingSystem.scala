8b5dc17e72dd6430b78504abe12894ad34800068
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2015-08-05 00:09:15 +0200

    Linux OS improvements

--- src/main/scala/pl/combosolutions/backup/dsl/internals/OperatingSystem.scala
+++ src/main/scala/pl/combosolutions/backup/dsl/internals/OperatingSystem.scala
@@ -2,12 +2,20 @@ package pl.combosolutions.backup.dsl.internals
 
 import org.apache.commons.lang3.SystemUtils._
 
+import scala.reflect.io.File
+
+import OperatingSystem._
+
 object OperatingSystem {
   val all = Seq(
     // Linux family
+    ArchSystem,
     DebianSystem,
+    FedoraSystem,
+    GentooSystem,
+    RedHatSystem,
     // MacOS family
-    MacOSOperatingSystem,
+    MacOSXOperatingSystem,
     // Windows family
     Windows7System,
     Windows8System,
@@ -15,6 +23,13 @@ object OperatingSystem {
   )
 
   lazy val current = all filter (_.isCurrent) head
+
+  // from http://linuxmafia.com/faq/Admin/release-files.html
+  def IS_OS_ARCH   = File("/etc/arch-release").exists
+  def IS_OS_DEBIAN = File("/etc/debian_version").exists
+  def IS_OS_FEDORA = File("/etc/fedora-release").exists
+  def IS_OS_GENTOO = File("/etc/gentoo-release").exists
+  def IS_OS_REDHAT = File("/etc/redhat-release").exists
 }
 
 // Operating systems families
@@ -26,11 +41,15 @@ abstract case class PosixSystem(override val name: String, override val isCurren
 // Linux family
 
 abstract case class LinuxSystem(override val name: String, override val isCurrent: Boolean) extends PosixSystem(name, isCurrent)
-case object DebianSystem extends LinuxSystem("Debian", IS_OS_LINUX)
+case object ArchSystem extends LinuxSystem("Arch", IS_OS_LINUX && IS_OS_ARCH)
+case object DebianSystem extends LinuxSystem("Debian", IS_OS_LINUX && IS_OS_DEBIAN)
+case object FedoraSystem extends LinuxSystem("Fedora", IS_OS_LINUX && IS_OS_FEDORA)
+case object GentooSystem extends LinuxSystem("Gentoo", IS_OS_LINUX && IS_OS_GENTOO)
+case object RedHatSystem extends LinuxSystem("Red Hat", IS_OS_LINUX && IS_OS_REDHAT)
 
 // MacOS family
 
-case object MacOSOperatingSystem extends PosixSystem("MacOS", IS_OS_MAC_OSX)
+case object MacOSXOperatingSystem extends PosixSystem("MacOS", IS_OS_MAC_OSX)
 
 // Windows family
 
