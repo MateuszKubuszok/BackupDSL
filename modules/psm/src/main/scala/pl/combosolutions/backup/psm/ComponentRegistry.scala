dd8b754fc5457f0632bb8bb4b32ed1f557f1c7a1
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2015-09-22 02:11:13 +0200

    Made sure that ComponentRegisty default impl is initialized only once

--- modules/psm/src/main/scala/pl/combosolutions/backup/psm/ComponentRegistry.scala
+++ modules/psm/src/main/scala/pl/combosolutions/backup/psm/ComponentRegistry.scala
@@ -4,7 +4,7 @@ import pl.combosolutions.backup.Logging
 import pl.combosolutions.backup.psm.elevation.{ ElevationServiceComponent, ElevationServiceComponentImpl }
 import pl.combosolutions.backup.psm.filesystem.{ FileSystemServiceComponent, FileSystemServiceComponentImpl }
 import pl.combosolutions.backup.psm.repositories.{ RepositoriesServiceComponent, RepositoriesServiceComponentImpl }
-import pl.combosolutions.backup.psm.systems.{ OperatingSystemComponentImpl, OperatingSystemComponent }
+import pl.combosolutions.backup.psm.systems.{ OperatingSystemComponent, OperatingSystemComponentImpl }
 
 trait ComponentRegistry extends AnyRef
   with OperatingSystemComponent
@@ -12,9 +12,27 @@ trait ComponentRegistry extends AnyRef
   with FileSystemServiceComponent
   with RepositoriesServiceComponent
 
-trait ComponentsHelper extends ComponentRegistry
+object ComponentRegistry extends ComponentRegistry
     with OperatingSystemComponentImpl
     with ElevationServiceComponentImpl
     with FileSystemServiceComponentImpl
-    with RepositoriesServiceComponentImpl { self: ComponentRegistry =>
+    with RepositoriesServiceComponentImpl
+    with Logging {
+
+  logger trace s"Operating System:     ${operatingSystem.getClass.getSimpleName}"
+  logger trace s"Elevation Service:    ${elevationService.getClass.getSimpleName}"
+  logger trace s"File System Service:  ${fileSystemService.getClass.getSimpleName}"
+  logger trace s"Repositories Service: ${repositoriesService.getClass.getSimpleName}"
+
+  trait ComponentRegistryImpl extends ComponentRegistry {
+
+    override def operatingSystem = ComponentRegistry.operatingSystem
+    override def elevationService = ComponentRegistry.elevationService
+    override def fileSystemService = ComponentRegistry.fileSystemService
+    override def repositoriesService = ComponentRegistry.repositoriesService
+  }
 }
+
+import pl.combosolutions.backup.psm.ComponentRegistry.ComponentRegistryImpl
+
+trait ComponentsHelper extends ComponentRegistry with ComponentRegistryImpl
