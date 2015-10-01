15fc16cac2df121762d0b30bc2e1b892f8f05064
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2015-10-02 00:43:38 +0200

    AptRepositories passed all platform tests

--- modules/psm/src/test/scala/pl/combosolutions/backup/psm/repositories/PlatformSpecificAdvisedTest.scala
+++ modules/psm/src/test/scala/pl/combosolutions/backup/psm/repositories/PlatformSpecificAdvisedTest.scala
@@ -4,10 +4,12 @@ import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import pl.combosolutions.backup.ReportException
 import pl.combosolutions.backup.psm.ComponentsHelper
-import pl.combosolutions.backup.psm.elevation.RemoteElevation
+import pl.combosolutions.backup.psm.elevation.{ NotElevated, RemoteElevation }
 import pl.combosolutions.backup.psm.repositories.posix.linux.AptRepositoriesServiceComponent.AptRepositoriesService
 import pl.combosolutions.backup.test.ElevationTestHelper
 import pl.combosolutions.backup.test.Tags.PlatformTest
+
+import scala.concurrent.duration.DurationInt
 
 class PlatformSpecificAdvisedTest
     extends Specification
@@ -24,11 +26,18 @@ class PlatformSpecificAdvisedTest
     case _ => ReportException onNotImplemented "Unknown repository"
   }
 
+  val testPackage: Package = repositoriesService match {
+    case repository: AptRepositoriesService => UnversionedPackage("grep")
+    case _ => ReportException onNotImplemented "Unknown repository"
+  }
+
+  val timeout = DurationInt(10) seconds
+
   "Current platform's repositories" should {
 
     "obtains repositories" in {
       // given
-      implicit val withElevation = RemoteElevation
+      implicit val withElevation = NotElevated
       implicit val cleaner = ElevationTestCleaner
 
       // when
@@ -62,6 +71,32 @@ class PlatformSpecificAdvisedTest
 
       // then
       result must beSome(true).await
+    } tag PlatformTest
+
+    "installs package" in {
+      // given
+      implicit val withElevation = RemoteElevation
+      implicit val cleaner = ElevationTestCleaner
+      val packages = List(testPackage)
+
+      // when
+      val result = repositoriesService installAll packages
+
+      // then
+      result must beSome(true).await(timeout = timeout)
+    } tag PlatformTest
+
+    "checks if all packages are installed" in {
+      // given
+      implicit val withElevation = RemoteElevation
+      implicit val cleaner = ElevationTestCleaner
+      val packages = List(testPackage)
+
+      // when
+      val result = repositoriesService areAllInstalled packages
+
+      // then
+      result must beSome(true).await(timeout = timeout)
     } tag PlatformTest
   }
 }
