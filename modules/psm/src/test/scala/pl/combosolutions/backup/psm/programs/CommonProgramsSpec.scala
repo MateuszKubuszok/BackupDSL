7eddd636d7c688054fe35a0684aa91a144ef1ac7
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2015-10-09 21:18:40 +0200

    CommonPrograms spec

---
+++ modules/psm/src/test/scala/pl/combosolutions/backup/psm/programs/CommonProgramsSpec.scala
@@ -0,0 +1,43 @@
+package pl.combosolutions.backup.psm.programs
+
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+
+class CommonProgramsSpec extends Specification with Mockito {
+
+  "GenericProgram" should {
+
+    "pass called program name and agruments without change" in {
+      // given
+      val name = "test-program"
+      val arguments = List("test1", "test2")
+
+      // when
+      val program = GenericProgram(name, arguments)
+
+      // then
+      program.name mustEqual name
+      program.arguments mustEqual arguments
+    }
+  }
+
+  "ProgramAlias" should {
+
+    "redirect name and arguments properly" in {
+      // given
+      val arguments = List("test1", "test2")
+      val expected = TestProgram(arguments ++ List("test3"))
+
+      // when
+      val alias = TestAlias(arguments)
+
+      // then
+      alias.name mustEqual expected.name
+      alias.arguments mustEqual expected.arguments
+    }
+  }
+
+  case class TestProgram(args: List[String]) extends Program[TestProgram]("test", args)
+
+  case class TestAlias(args: List[String]) extends ProgramAlias[TestAlias, TestProgram](TestProgram(args ++ List("test3")))
+}
