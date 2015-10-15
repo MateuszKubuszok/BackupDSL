cb2bccb429051b9c77c3e1bd1a1bbefd8f2d234f
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2015-10-15 13:53:25 +0200

    Small style change

--- project/BackupProject.scala
+++ project/BackupProject.scala
@@ -2,25 +2,25 @@ import sbt._
 
 object BackupProject extends Build with Settings with Dependencies {
 
-  lazy val common = project.from("common").
-    configureCommon
+  lazy val common = project.from("common")
+    .configureCommon
 
-  lazy val psm = project.from("psm").
-    configureCommon.
-    configurePlatform.
-    configureFunctional.
-    configureUnit.
-    dependsOnProjects(common)
+  lazy val psm = project.from("psm")
+    .configureCommon
+    .configurePlatform
+    .configureFunctional
+    .configureUnit
+    .dependsOnProjects(common)
 
-  lazy val tasks = project.from("tasks").
-    configureCommon.
-    configureFunctional.
-    configureUnit.
-    dependsOnProjects(common, psm)
+  lazy val tasks = project.from("tasks")
+    .configureCommon
+    .configureFunctional
+    .configureUnit
+    .dependsOnProjects(common, psm)
 
-  lazy val dsl = project.from("dsl").
-    configureCommon.
-    configureFunctional.
-    configureUnit.
-    dependsOnProjects(common, psm, tasks)
+  lazy val dsl = project.from("dsl")
+    .configureCommon
+    .configureFunctional
+    .configureUnit
+    .dependsOnProjects(common, psm, tasks)
 }
